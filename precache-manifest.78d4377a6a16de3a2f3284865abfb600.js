7b229660f79a64f7dea99dfc30fd3199f2aa7ff4
KP Parrish <57117452+KParrish193@users.noreply.github.com>
2020-05-28 16:43:27 -0600

    Updates

---
+++ precache-manifest.78d4377a6a16de3a2f3284865abfb600.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "579036c6aade49ba64b155665a8a732e",
+    "url": "/%3Cgithub-repo-name%3E/index.html"
+  },
+  {
+    "revision": "5d78cd67847c2d7e599e",
+    "url": "/%3Cgithub-repo-name%3E/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0169d30f241ac331fad0",
+    "url": "/%3Cgithub-repo-name%3E/static/js/2.af2f9459.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/%3Cgithub-repo-name%3E/static/js/2.af2f9459.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d78cd67847c2d7e599e",
+    "url": "/%3Cgithub-repo-name%3E/static/js/main.7609d537.chunk.js"
+  },
+  {
+    "revision": "c90c31a4ce0db3b6c250",
+    "url": "/%3Cgithub-repo-name%3E/static/js/runtime-main.df402b52.js"
+  },
+  {
+    "revision": "6497df478e5d6257455e25823e4be42e",
+    "url": "/%3Cgithub-repo-name%3E/static/media/Griddys_Doughnuts.6497df47.png"
+  },
+  {
+    "revision": "1f94ecb0aa20a8a87a0022fa40917651",
+    "url": "/%3Cgithub-repo-name%3E/static/media/griddysin.1f94ecb0.jpg"
+  },
+  {
+    "revision": "225f9b084ba1b3ef1e2a9c0506149856",
+    "url": "/%3Cgithub-repo-name%3E/static/media/menu1.225f9b08.jpg"
+  }
+]);
